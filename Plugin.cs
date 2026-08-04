1b582dd49ed7d37a1b44a1f4654724686b08f9fd
Storption <shaunchandler52@gmail.com>
2026-08-04 03:51:03 +0100

    Bump plugin version to 1.0.1
    Updated the version number in Plugin.cs from 1.0.0 to 1.0.1. No other changes were made.

--- Plugin.cs
+++ Plugin.cs
@@ -30,7 +30,7 @@
         public override Version RequiredExiledVersion { get; } = new Version(9, 14, 2);
 
         /// <inheritdoc/>
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
 
         /// <inheritdoc/>
         public override void OnEnabled()
